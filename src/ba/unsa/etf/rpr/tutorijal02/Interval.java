6919b8ca03faf8595f90ef81aa1d1409fa335226
Eldar Civgin <ecivgin1@etf.unsa.ba>
2020-11-06 17:47:59 +0100

    Prvi commit, uradjeni konstruktori i metode isNull kao i isIn.

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -1,6 +1,40 @@
 package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
+    private double Pocetna, Krajnja;
+    private boolean PripadaP, PripadaK;
 
+    public Interval(double Pocetna, double Krajnja, boolean PripadaP, boolean PripadaK) throws IllegalArgumentException {
+        if (Pocetna > Krajnja) {
+            throw new IllegalArgumentException("Pocetna tacka je veca od krajnje");
+        }
+        this.Pocetna = Pocetna;
+        this.Krajnja = Krajnja;
+        this.PripadaP = PripadaP;
+        this.PripadaK = PripadaK;
+    }
 
+    public Interval() {
+        this.Pocetna = 0;
+        this.Krajnja = 0;
+        this.PripadaP = false;
+        this.PripadaK = false;
+    }
+
+    boolean isNull() {
+        return Pocetna == 0 && Krajnja == 0 && !PripadaP && !PripadaK;
+    }
+
+    boolean isIn(double Parametar) {
+        if (PripadaP)
+            if (PripadaK)
+                return Parametar >= Pocetna && Parametar <= Krajnja;
+            else
+                return Parametar >= Pocetna && Parametar < Krajnja;
+        else
+        if (PripadaK)
+            return Parametar > Pocetna && Parametar <= Krajnja;
+        else
+            return Parametar > Pocetna && Parametar < Krajnja;
+    }
 }
